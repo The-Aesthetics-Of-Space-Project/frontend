254df40dcece7ccdbda4cd7e25a2aa76c908ff8b
jbg <kjbg4565388@gmail.com>
2024-05-03 06:54:32 +0900

    feat: 이미지 사진 레이아웃 및 결과분석 클릭시 모달창 이동

--- src/views/InteriorGuideView.vue
+++ src/views/InteriorGuideView.vue
@@ -2,8 +2,10 @@
 <template>
   <div class="container">
     <div class="left-section">
-      <h1>Interior Design Guide</h1>
-      <p>{{ textContent }}</p>
+      <h1 class="font-h1"><span style="position: relative; left:-80px;">마음에 드는</span><br><span style="position: relative; left:-25px; color:darkgreen">
+        인테리어 이미지를</span> <br>
+        <span style="position: relative; left:-60px;">선택해 주세요.</span></h1>
+      <p class="guide-textcontent">{{ textContent }}</p>
     </div>
     <div class="right-section">
       <div class="image-gallery">
@@ -13,15 +15,20 @@
       </div>
       <button @click="analyzeSelectedImages" :disabled="selectedImages.length !== 3">Analyze</button>
     </div>
-    <div class="analysis-section" v-if="showAnalysis">
+    <modal v-if="showModal" @close="closeModal">
       <h2>Preference Analysis</h2>
       <p>{{ analysisResult }}</p>
-    </div>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from '@/components/Modal.vue'
+
 export default {
+  components: {
+    Modal
+  },
   data() {
     return {
       textContent: "This is the text content for the interior design guide.",
@@ -29,36 +36,61 @@ export default {
         { src: 'image1.jpg', alt: 'Image 1' },
         { src: 'image2.jpg', alt: 'Image 2' },
         { src: 'image3.jpg', alt: 'Image 3' },
+        { src: 'image4.jpg', alt: 'Image 4' },
+        { src: 'image5.jpg', alt: 'Image 5' },
+        { src: 'image6.jpg', alt: 'Image 6' },
+        { src: 'image7.jpg', alt: 'Image 7' },
+        { src: 'image8.jpg', alt: 'Image 8' },
+        { src: 'image9.jpg', alt: 'Image 9' },
+        { src: 'image10.jpg', alt: 'Image 10' },
+        { src: 'image11.jpg', alt: 'Image 11' },
+        { src: 'image12.jpg', alt: 'Image 12' }
         // Add more images here
       ],
       selectedImages: [],
       showAnalysis: false,
-      analysisResult: ""
+      analysisResult: "",
+      showModal: false
     }
   },
   methods: {
     selectImage(index) {
-      if (this.selectedImages.length < 3) {
+      const selectedIndex = this.selectedImages.indexOf(index);
+      if (selectedIndex === -1) {
         this.selectedImages.push(index);
       } else {
-        this.selectedImages.splice(0, 1, index);
+        this.selectedImages.splice(selectedIndex, 1);
       }
-    },
+  },
     analyzeSelectedImages() {
       // Perform analysis on the selected images
-      this.analysisResult = "Based on your selections, we recommend the following interior design style...";
-      this.showAnalysis = true;
+      this.analysisResult = "Based on your selections, we recommend the following interior design style..."
+      this.showModal = true
+    },
+    closeModal() {
+      this.showModal = false
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
 .container {
   display: flex;
   justify-content: space-between;
+  height:600px;
 }
 
+.font-h1{
+  position: relative;
+  top:200px;
+  font-weight:bolder;
+}
+.guide-textcontent{
+  position: relative;
+  top:200px;
+  left:-10px;
+}
 .left-section {
   flex: 1;
   padding: 20px;
