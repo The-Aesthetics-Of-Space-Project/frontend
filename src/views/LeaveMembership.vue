b753f7e26e6fb9ca6d4853267c11bef42155384a
EunJinCHO <till5941@gmail.com>
2024-05-17 23:00:53 +0900

    fix: 마이페이지(정보 수정, 탈퇴 페이지) UI 수정

--- src/views/LeaveMembership.vue
+++ src/views/LeaveMembership.vue
@@ -1,27 +1,28 @@
 <template>
   <div id="leave">
-    <div class="content-container">
-      <section class="content-wrapper">
-        <a>
-          탈퇴 후 아이디 <span style="color: #80C85F"> xxx@ooo.com </span> 으로 재가입 시 일주일의 시간이 걸리며<br>
-          해당 아이디로 작성한 데이터는 복구할 수 없습니다.<br>
-          작성한 게시물은 탈퇴 후 삭제할 수 없습니다.<br>
-        </a>
-      </section>
-      <section class="agree-container">
-        <div class="agree-wrapper form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-          <label class="form-check-label" for="flexCheckDefault">
-            안내 사항을 확인하였으며, 이에 동의합니다.
-          </label>
-        </div>
-      </section>
+    <div class="leave-container">
+        <section class="leave-content-wrapper">
+          <a>
+            탈퇴 후 아이디 <span style="color: #80C85F"> xxx@ooo.com </span> 으로 재가입 시 일주일의 시간이 걸리며<br>
+            해당 아이디로 작성한 데이터는 복구할 수 없습니다.<br>
+            작성한 게시물은 탈퇴 후 삭제할 수 없습니다.<br>
+          </a>
+        </section>
+        <section class="agree-container">
+          <div class="agree-wrapper form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              안내 사항을 확인하였으며, 이에 동의합니다.
+            </label>
+          </div>
+        </section>
 
-      <div class="d-grid gap-2">
-        <el-button type="button" class="btn-loginIn" id="modifyButton"> 확인 </el-button>
-      </div>
+        <section class="agree-btn-wrapper">
+          <button type="button" class="btn-agree" id="agreeButton"> 확인 </button>
+        </section>
 
     </div>
+
 
   </div>
 </template>
@@ -36,21 +37,36 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
+  position: relative;
+  width: 100%;
+  height: 528px;
+  margin: 0;
+  align-content: center;
 }
-.content-container{
-  margin: 19% 26%;
+.leave-container{
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.leave-content-wrapper{
+  position: relative;
+  width: 45%;
+  height: 25%;
+  top: 120px;
+  margin: auto;
   font-weight: 550;
   color: #B1B3B9;
   text-decoration: none;
   font-size: 24px;
 }
-.content-wrapper{
-  margin: 15px 20px;
-}
 .agree-container{
-  margin: 5% 2.5%;
-  font-weight: 380;
-  color: RGB(0,0,0,52%);
+  position: relative;
+  width: 28%;
+  top: 120px;
+  left: -140px;
+  margin: auto;
+  font-weight: 500;
+  color: RGB(0,0,0,50%);
   text-decoration: none;
   font-size: 20px;
 }
@@ -60,9 +76,23 @@
   box-shadow: #80C85F;
   accent-color: green;
 }
-.d-grid{
-  text-align: center;
-  width: 420px;
-  margin: 80px 220px;
+.agree-btn-wrapper{
+  position: relative;
+  width: 20%;
+  height: 11%;
+  top: 160px;
+  left: 60px;
+  align-content: center;
+  margin: auto;
+}
+.btn-agree{
+  width: 80%;
+  height: 80%;
+  border: none;
+  border-radius: 12px;
+  background-color: #80C85F;
+  font-size: 19px;
+  font-weight: 550;
+  color: floralwhite;
 }
 </style>
