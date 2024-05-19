e6fcb558c53b26f3160b620f209cdef02a396e44
jbg <kjbg4565388@gmail.com>
2024-05-19 23:24:17 +0900

    fix: 레이아웃 수정

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -2,15 +2,15 @@
   <div class="community">
     <div class="main-sc-container">
       <div class="title-container">
-        <h1 style="font-family: MyCustomFont4; color: #333333; font-size: 31px; ">모두를 위한 커뮤니티</h1>
+        <h1 style=" color: #333333; font-size: 31px;   font-family: MyCustomFont2; ">모두를 위한 커뮤니티</h1>
       </div>
       <div class="grid-container">
         <div class="image-sc-container">
           <router-link to="/interiorguide">
             <img src="@/assets/community_image_list/ckdwha.png" alt="인테리어 사진" class="image-sc">
           </router-link>
-          <div class="title-sc" style="position: relative; left:-135px;">일반 게시판</div>
-          <div class="title-sc" style="font-size: 19px; position: relative; left:-55px; font-family: MyCustomFont5;">다양한 주제의 소통 공간입니다.</div>
+          <div class="title-sc" style="position: relative; left:-150px;">게시판</div>
+          <div class="title-sc" style="font-size: 19px; position: relative; left:-55px;">다양한 주제의 소통 공간입니다.</div>
 
         </div>
 
@@ -18,8 +18,8 @@
           <router-link :to="isUserLogin ? '/GeneralBoard':'/login'">
             <img src="@/assets/community_image_list/board_write.png" alt="유저들의 콘텐츠" class="image-sc">
           </router-link>
-          <div class="title-sc" style="position: relative; left:-105px;">일반 게시판 글쓰기</div>
-          <div class="title-sc" style="font-size: 19px; position: relative; left:-55px; font-family: MyCustomFont5;"> 생각과 경험을 나누는 창입니다.</div>
+          <div class="title-sc" style="position: relative; left:-120px;">게시판 글쓰기</div>
+          <div class="title-sc" style="font-size: 19px; position: relative; left:-55px; "> 생각과 경험을 나누는 창입니다.</div>
 
         </div>
 
@@ -27,17 +27,17 @@
           <router-link to="/Community">
             <img src="@/assets/community_image_list/competition.png" alt="커뮤니티" class="image-sc">
           </router-link>
-          <div class="title-sc" style="position: relative; left:-125px;">공모전 게시판</div>
-          <div class="title-sc" style="font-size: 19px; position: relative; left:-25px; font-family: MyCustomFont5;">창의력에 대한 도전을 펼칠 수 있습니다.</div>
+          <div class="title-sc" style="position: relative; left:-150px;">공모전</div>
+          <div class="title-sc" style="font-size: 19px; position: relative; left:-25px; ">창의력에 대한 도전을 펼칠 수 있습니다.</div>
 
         </div>
 
         <div class="image-sc-container">
           <router-link to="/Community">
-            <img src="@/assets/community_image_list/comptetition_write.png" alt="커뮤니티" class="image-sc">
+            <img src="@/assets/community_image_list/competition_write.png" alt="커뮤니티" class="image-sc">
           </router-link>
-          <div class="title-sc" style="position: relative; left:-95px;">공모전 게시판 글쓰기</div>
-          <div class="title-sc" style="font-size: 19px; position: relative; left:-45px; font-family: MyCustomFont5;">아이디어를 경쟁의 무대에 올립니다.</div>
+          <div class="title-sc" style="position: relative; left:-120px;">공모전 글쓰기</div>
+          <div class="title-sc" style="font-size: 19px; position: relative; left:-45px; ">아이디어를 경쟁의 무대에 올립니다.</div>
         </div>
       </div>
     </div>
@@ -119,9 +119,4 @@ export default {
   font-weight: bolder;
 }
 
-.description-sc {
-  color: #666;
-  margin-top: 10px;
-  font-size: 0.9em;
-}
 </style>
