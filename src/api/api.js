a8254aca25e345c05e01904c6459ddaccf67c554
EunJinCHO <till5941@gmail.com>
2024-05-22 07:42:23 +0900

    feat: 마이페이지 회원 탈퇴 데이터 요청 코드 구현

--- src/api/api.js
+++ src/api/api.js
@@ -5,18 +5,50 @@ export const api = {
     getUser(args) {
         return service.get(args)
     },
-    /* 사용자 ID로 목록 조회 */
+    /* 마이페이지 유저 정보 조회 요청 */
     getUserInfo(args){
-        console.log("args: "+args);
         return service.get(args)
     },
-    /* 팔로워/팔로잉 목록 조회 */
+    /* 팔로워, 팔로잉 리스트 전체 조회 */
     getFollow(args) {
         return service.get(args)
     },
-    setUser(args,param) {
-        return service.post(args, param)
+    /* 서버쪽에 이미지 요청 */
+    getImg(args){
+        return service.get(args)
     },
+    /* 게시글 전체 목록 조회*/
+    getPost(args){
+        return service.get(args)
+    },
+    setUser(args, params) { // url 매개변수를 첫 번째 인자로 추가
+        return service.post(args,params)
+    },
+    /* 게시글 작성 이미지 요청(첨부할 때) */
+    setGeneralUser(args, params){
+      return service.post(args, params)
+    },
+    /* 좋아요 등록 */
+    setLike(args, params){
+        return service.post(args, params)
+    },
+    /* 좋아요 취소 */
+    unSetLike(args, params){
+        return service.post(args, params)
+    },
+    /* 스크랩 등록 */
+    setScrap(args, params){
+        return service.post(args, params)
+    },
+    /* 댓글 등록 */
+    setComment(args, params){
+        return service.post(args, params)
+    },
+    /* 스크랩 취소 */
+    unSetScrap(args, params){
+        return service.post(args, params)
+    },
+    /* 마이페이지 유저 정보 수정 */
     updateUser(args, param) { // args와 param이 동시 존재하는 경우
         return service.put(args, param)
     },
@@ -34,13 +66,9 @@ export const testApi = {
     getTests() {
         return service.get(`${API_URL}${API.tests}`)
     },
-    /* test 등록하기
-     * @param { Object } args
-     * @param { string } args.args1 - 이것
-     * @param { string } args.args2 - 저것
-     * @param { string } args.args3 - 그것
-     */
+
     setTest(args) {
-        return service.post(`${API_URL}${API.test(args)}`)
-    }
+        // URL과 함께 args를 바디로 전달
+        return service.post(`${API_URL}${API.test}`, args);
+    },
 }
