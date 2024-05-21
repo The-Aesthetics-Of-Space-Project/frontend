edabb544e9c57c008d17d006d09ab1d95540f17b
EunJinCHO <till5941@gmail.com>
2024-05-22 07:57:09 +0900

    fix: 로그인 요청 버그 수정 및 닉네임 세션 값 설정 수정

--- src/store/index.js
+++ src/store/index.js
@@ -8,22 +8,38 @@ export default new Vuex.Store({
     plugins: [
         createPersistedState({
             key: 'my-app-state', // localStorage에 저장될 키 이름
-            paths: ['userId'] // 저장할 state 속성 지정
+            paths: ['userId','nickname','profile'] // 저장할 state 속성 지정
         })
     ],
     state: {
-        userId: ''
+        userId: '',
+        nickname: '',
+        profile: '',
     },
     getters: {
         isLogin(state) {
             return state.userId !== '';
+        },
+        userNickname(state) { // 닉네임을 가져오는 getter 추가
+            return state.nickname;
+        },
+        userProfile(state) { // 프로필을 가져오는 getter 추가
+            return state.profile; // 'profifle' 오타를 'profile'로 수정
         }
     },
     mutations: {
         setUserId(state, userId) {
             state.userId = userId;
         },
+        setUserNickname(state, nickname) {
+            state.nickname = nickname;
+        },
+        setUserProfile(state,profile){
+            state.profile = profile;
+        },
         clearUsername(state) {
+            state.nickname='';
+            //state.profile='';
             state.userId = '';
         }
     }
