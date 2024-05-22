c7113e5bc325819fd5772f8c763d8552200e5976
jbg <kjbg4565388@gmail.com>
2024-05-23 08:04:52 +0900

    feat: 기초 레이아웃

--- src/views/ObjectRecognitionView.vue
+++ src/views/ObjectRecognitionView.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="objectrecognition">
-    <div class="container">
+    <div class="objectrecognition-container">
       <div class="left-column">
-        <img  alt="Your Image" class="rounded-image">
+        <img src="YourImage.jpg" alt="Your Image" class="rounded-image">
       </div>
       <div class="right-column">
-        <span>당신의 인테리어 스타일은</span> <br>
-        <span>머시깽이</span><br>
-        <span>이네요 !</span><br>
+        <span class="header-text">당신의 인테리어 스타일은</span>
+        <span class="style-name">머시깽이</span>
+        <span class="confirmation-text">이네요 !</span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'objectrecognition'
 }
 </script>
+
 <style>
 .objectrecognition {
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 50px 0; /* 상하 패딩 추가 */
 }
 
-.container {
+.objectrecognition-container {
   display: flex;
-  width: 80%;
+  width: 60%;
   background-color: white;
-
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5); /* 그림자 추가 */
+  border-radius: 16px; /* 테두리 둥글게 */
+  overflow: hidden; /* 내용이 테두리를 넘지 않도록 */
+  height: 800px;
 }
 
 .left-column {
@@ -49,10 +52,39 @@ export default {
   flex: 1;
   padding: 20px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  font-size: 18px;
-  font-weight: bold;
+  background-color: #ffffff; /* 배경색 변경 */
+}
+
+.header-text, .style-name, .confirmation-text {
   color: #333;
+  text-align: center;
+  position: relative;
+  top:-200px;
+}
+
+.header-text {
+  font-size: 22px;
+  margin-bottom: 15px;
+  font-weight: 500; /* 글꼴 무게 조정 */
+}
+
+.style-name {
+  font-size: 28px;
+  font-weight: bold;
+  color: #4A90E2; /* 스타일 이름 색상 변경 */
+  margin-bottom: 10px;
+}
+
+.confirmation-text {
+  font-size: 22px;
+  font-weight: 400;
+}
+
+/* 각각의 줄 간격 조정 */
+.right-column span {
+  margin-bottom: 10px; /* 줄 간격 조정 */
 }
 </style>
